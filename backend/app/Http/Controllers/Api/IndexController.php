0d5b09edcf2ccb4db1f519d3af2d8e45866bdd6c
osster <Osster1488>
2021-03-04 13:06:37 +0300

    env configuration with sockets

---
+++ backend/app/Http/Controllers/Api/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index(Request $request): JsonResponse
+    {
+        return response()->json(["result" => "OK"]);
+    }
+
+    public function test(Request $request): JsonResponse
+    {
+        return response()->json(["result" => "OK", "type" => "post"]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::prefix("v1")
+    ->group(function () {
+
+        Route::get("/", [\App\Http\Controllers\Api\IndexController::class, "index"]);
+        Route::post("/test", [\App\Http\Controllers\Api\IndexController::class, "test"]);
+
+    });
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
